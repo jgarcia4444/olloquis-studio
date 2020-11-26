96fd25c4e4f9f88ee775b850cf07bba69b02bdc8
github username <bballwiz4@gmail.com>
2020-11-26 00:48:09 -0800

    Started to add the pproducts to the shop page.

---
+++ src/ProductData.js
@@ -0,0 +1,29 @@
+
+import Adhesive from './imgs/products/Adhesive.png';
+import Lashes8 from './imgs/products/Lashes8.png';
+import Lashes9 from './imgs/products/Lashes9.png';
+import Lashes10 from './imgs/products/Lashes10.png';
+import Lashes11 from './imgs/products/Lashes11.png';
+import Lashes12 from './imgs/products/Lashes12.png';
+import Lashes13 from './imgs/products/Lashes13.png';
+import Lashes14 from './imgs/products/Lashes14.png';
+import Lashes15 from './imgs/products/Lashes15.png';
+import Lashes16 from './imgs/products/Lashes16.png';
+import Lashes17 from './imgs/products/Lashes17.png';
+
+const ProductData =
+    [
+        {name: "Adhesive", imgSRC: Adhesive},
+        {name: "Lashes Size 8", imgSRC: Lashes8},
+        {name: "Lashes Size 9", imgSRC: Lashes9},
+        {name: "Lashes Size 10", imgSRC: Lashes10},
+        {name: "Lashes Size 11", imgSRC: Lashes11},
+        {name: "Lashes Size 12", imgSRC: Lashes12},
+        {name: "Lashes Size 13", imgSRC: Lashes13},
+        {name: "Lashes Size 14", imgSRC: Lashes14},
+        {name: "Lashes Size 15", imgSRC: Lashes15},
+        {name: "Lashes Size 16", imgSRC: Lashes16},
+        {name: "Lashes Size 17", imgSRC: Lashes17},
+    ]
+
+export default ProductData;
--- src/components/shop/ShopContainer.js
+++ src/components/shop/ShopContainer.js
@@ -1,13 +1,63 @@
 import React, { Component } from 'react';
 import './ShopContainer.css';
+import ProductData from '../../ProductData';
 
 class ShopContainer extends Component {
+
+    state = {
+        products: ProductData
+    }
+
+    renderProducts = (products) => {
+        console.log(products)
+        return products.map((product, i) => {
+            if (i % 2 === 0 && i !== products.length - 1) {
+                let nextProduct = products[i + 1]
+                return (
+                    <div className="row">
+                        <div className="col-6 product-col">
+                            <div className="product-container">
+                                <h3>{nextProduct.name}</h3>
+                                <img className="product-image" src={nextProduct.imgSRC} alt={nextProduct.name} />
+                            </div>
+                        </div>
+                        <div className="col-6 product-col">
+                            <div className="product-container">
+                                <h3>{product.name}</h3>
+                                <img className="product-image" src={product.imgSRC} alt={product.name} />
+                            </div>
+                        </div>
+                    </div>
+                )  
+            } else if (i === products.length - 1) {
+                return (
+                    <div className="row text-center">
+                        <div className="col-12 product-col">
+                            <h3>{product.name}</h3>
+                            <img className="product-image" src={product.imgSRC} alt={product.name} />
+                        </div>
+                    </div>
+                )
+            }
+        })
+    }
 
     render() {
 
         return (
             <div className="container shop-container">
-                Shop Container
+                <div className="row text-center">
+                    <div className="col-12">
+                        <h1>Shop Products</h1>
+                    </div>
+                </div>
+                <div className="row text-center">
+                    <div className="col-12">
+                        <div className="container products-container">
+                            {this.renderProducts(this.state.products)}
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
