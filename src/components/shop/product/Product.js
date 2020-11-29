b2e499c17d45f1aee18f413e65ea352b6e87b224
github username <bballwiz4@gmail.com>
2020-11-29 09:30:36 -0800

    Products now have a wquantity specifier along with an add to cart button. If the add to cart button is hit while the quantity is set to zero an error modal is displayed over the product telling the user what to do with a close button.

--- src/components/shop/product/Product.js
+++ src/components/shop/product/Product.js
@@ -5,15 +5,93 @@ import addToCart from '../../../redux/actions/addToCart'
 
 class Product extends Component {
 
+    state = {
+        quantitySelect: 0,
+        showErrorModal: false
+    }
+
+    handleSelectChange = (e) => {
+        this.setState({
+            [e.target.name]: parseInt(e.target.value)
+        })
+    }
+
+    handleAddToCart = () => {
+        if (this.state.quantitySelect === 0) {
+            this.setState({
+                ...this.state,
+                showErrorModal: true
+            })
+        }
+    }
+
+    renderQuantityOptions = () => {
+        const quantityRange = [...Array(21).keys()];
+        return (
+            <select onChange={this.handleSelectChange} name="quantitySelect" className="form-control quantity-select" value={this.state.quantitySelect}>
+                {quantityRange.map(quantity => {
+                    return <option className="quantity-selection" name={"quantity_" + quantity} value={quantity}>{quantity}</option>
+                })}
+            </select>
+        )
+        
+    }
+
+    setModalToFalse = () => {
+        this.setState({
+            ...this.state,
+            showErrorModal: false
+        })
+    }
+
+    closeErrorModal = (e) => {
+        let parentContainer = e.target.parentNode
+        parentContainer.classList.add("disappear")
+        setTimeout(this.setModalToFalse, 500)
+    }
+
     render() {
-
+        
         const {product} = this.props
-
         return (
             <div className='product-container'>
+            {this.state.showErrorModal ?
+                <div className="container-fluid quantity-error-container">
+                    <div onClick={this.closeErrorModal} className="close-button-container">
+                        X
+                    </div>
+                    <div className="quantity-error-message">
+                        Please specify the quantity
+                    </div>
+                </div>
+             : null}
+            
                 <div className="row text-center">
-                    <div className="col-8 product-col">
+                    <div className="col-12 product-col">
                         <img className="product-image" src={product.imgSRC} alt={product.name} />
+                    </div>
+                </div>
+                <div className="container product-details-container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h4 className="product-name">
+                                {product.name}
+                            </h4>
+                        </div>
+                        <div className="col-12">
+                            <p className="product-details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsum explicabo, beatae voluptate rem aliquam maiores obcaecati, aspernatur et laudantium officia provident aperiam fugit eveniet vel quos id eaque tempora.</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-6">
+                            <label>
+                                Quantity:                                
+                            </label>
+                            {this.renderQuantityOptions()}
+                        </div>
+                        <div className="col-6">
+                            <button onClick={this.handleAddToCart} className="add-to-cart-button">Add To Cart</button>
+                        </div>
                     </div>
                 </div>
             </div>
