d214ebf96615b964fc554f9f4aae4e480b7598ef
github username <bballwiz4@gmail.com>
2020-11-30 19:00:57 -0800

    When the cart is cclicked it now displays what is in the cart as well as the quantity.

--- src/components/cartDetailsContainer/CartDetailsContainer.js
+++ src/components/cartDetailsContainer/CartDetailsContainer.js
@@ -1,15 +1,29 @@
 import './CartDetailsContainer.css'
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class CartDetailsContainer extends Component {
 
     render() {
+        console.log(this.props.cart)
         return (
             <div className="container cart-details-container">
                 <h1>Cart Details</h1>
+                {this.props.cart.map(item => {
+                    return <h3>{item.product.name} quantity: {item.quantity}</h3>
+                })}
             </div>
         )
     }
 }
 
-export default CartDetailsContainer;
+const mapStateToProps = state => {
+    return {
+        cart: state.cartItems
+    }
+} 
+
+export default connect(
+    mapStateToProps,
+    null
+)(CartDetailsContainer);
