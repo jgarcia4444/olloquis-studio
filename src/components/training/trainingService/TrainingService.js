74c5c407c31470f1cb712247055af570626ee5fd
github username <bballwiz4@gmail.com>
2020-12-02 18:29:19 -0800

    The signup form now sends a post request to the backend and responds accordingly with a message over the selected training.

--- src/components/training/trainingService/TrainingService.js
+++ src/components/training/trainingService/TrainingService.js
@@ -6,13 +6,49 @@ import SignupForm from './signupForm/SignupForm';
 class TrainingService extends Component {
 
     state = {
-        showSignupForm: false
+        showSignupForm: false,
+        showConfirmation: false,
+        confirmationMessage: ""
+    }
+
+    setupConfirmationMessage = (message) => {
+        this.setState({
+            showSignupForm: false,
+            showConfirmation: true,
+            confirmationMessage: message
+        })
+    }
+
+    closeConfirmationMessage = (e) => {
+        let parentContainer = e.target.parentNode;
+        parentContainer.classList.add("close-signup-container");
+        setTimeout(this.wipeConfirmationMessage, 700);
+    }
+
+    wipeConfirmationMessage = () => {
+        this.setState({
+            showConfirmation: false,
+            confirmationMessage: ""
+        })
     }
 
     render() {
 
         return (
             <div className="container training-service-container">
+            {
+                this.state.showConfirmation ? 
+                    <div className="container-fluid message-after-signup-container">
+                        <span onClick={this.closeConfirmationMessage} className="close-signup-message">X</span>
+                        <div className="row">
+                            <div className="col-6 confirm-message-col">
+                                {this.state.confirmationMessage}
+                            </div>
+                        </div>
+                    </div>
+                    : 
+                    null
+            }
                 <div className="row">
                     <div className="col-12">
                         <img className="training-image" src={Lashes} alt="Lashes" />
@@ -24,7 +60,7 @@ class TrainingService extends Component {
                     </div>
                     <div className="col-12">
                         { this.state.showSignupForm ?
-                        <SignupForm />
+                        <SignupForm setupConfirmationMessage={this.setupConfirmationMessage} name={this.props.trainingName}/>
                             : 
                         <button onClick={() => this.setState({showSignupForm: true})} className="btn btn-primary signup-button">Signup</button>
                         }
--- src/components/training/trainingService/signupForm/SignupForm.js
+++ src/components/training/trainingService/signupForm/SignupForm.js
@@ -6,7 +6,7 @@ class SignupForm extends Component {
     state = {
         fName: "",
         lName: "",
-        licensed: false
+        licensed: false,
     }
 
     handleInputChange = (e) => {
@@ -16,8 +16,48 @@ class SignupForm extends Component {
         })
     }
 
-    handleSignupSubmit = () => {
-        console.log("Hello World")
+    handleSignupSubmit = (e) => {
+        e.preventDefault();
+        let backendObject = this.setUpStateForBackend();
+        let options = this.optionsForSignupPost(backendObject);
+        this.postToBackend(options)
+    }
+
+    postToBackend = (options) => {
+        fetch("http://localhost:3000/trainings", options)
+            .then(res => res.json())
+            .then(data => {
+                if (data.success) {
+                    let confirmMessage = `${this.state.fName} ${this.state.lName}, thank you for signing up for ${this.props.name}. We will email you with further instructions.`
+                    this.props.setupConfirmationMessage(confirmMessage)
+                } else {
+                    let confirmMessage = "Sorry there was an error trying to sign you up for training."
+                    this.props.setupConfirmationMessage(confirmMessage)
+                }
+            })
+    }
+
+    optionsForSignupPost = (signupObject) => {
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(signupObject)
+        }
+        return options
+    }
+
+    setUpStateForBackend = () => {
+        const backendSignupObject = {
+            first_name: this.state.fName,
+            last_name: this.state.lName,
+            email: this.state.email,
+            licensed: this.state.licensed,
+            training_name: this.props.name
+        }
+        return backendSignupObject;
     }
 
     handleCheckboxClick = () => {
@@ -34,12 +74,18 @@ class SignupForm extends Component {
                 <form onSubmit={this.handleSignupSubmit}>
                     <div className="row form-group">
                         <div className="col-6">
-                            <label htmlFor="fName">First Name</label>
-                            <input id="fName" className="form-control" type="text" name="fName" value={this.state.fName} onChange={this.handleInputChange} />
+                            <label htmlFor={"fName_" + this.props.name}>First Name</label>
+                            <input id={"fName_" + this.props.name} className="form-control" type="text" name="fName" value={this.state.fName} onChange={this.handleInputChange} />
                         </div>
                         <div className="col-6">
-                            <label htmlFor="lName">Last Name</label>
-                            <input id="lName" className="form-control" type="text" name="lName" value={this.state.lName} onChange={this.handleInputChange} />
+                            <label htmlFor={"lName_" + this.props.name}>Last Name</label>
+                            <input id={"lName_" + this.props.name} className="form-control" type="text" name="lName" value={this.state.lName} onChange={this.handleInputChange} />
+                        </div>
+                    </div>
+                    <div className="row form-group">
+                        <div className="col-12 signup-email-col">
+                            <label htmlFor={"email_" + this.props.name}>Email</label>
+                            <input type="email" name="email" className="form-control" id={"email_" + this.props.name} value={this.state.eamil} onChange={this.handleInputChange} />
                         </div>
                     </div>
                     <div className="row form-group">
@@ -48,6 +94,11 @@ class SignupForm extends Component {
                         </div>
                         <div className="col-10 licensed-question">
                             Are you a licensed Cosmetologist or Esthetician
+                        </div>
+                    </div>
+                    <div className="row form-group">
+                        <div className="col-12 signup-submit-col">
+                            <button type="submit" className="btn btn-primary signup-submit-button">Submit</button>
                         </div>
                     </div>
                 </form>
