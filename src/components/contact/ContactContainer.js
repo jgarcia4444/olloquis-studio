1aade03bfa0e68e6c3c00fb750ac3751c2d30fd4
github username <bballwiz4@gmail.com>
2020-12-05 16:13:19 -0800

    Took out medium titles and added in callable phone number.

--- src/components/contact/ContactContainer.js
+++ src/components/contact/ContactContainer.js
@@ -21,13 +21,18 @@ class ContactContainer extends Component {
                     <div className="col-4">
                         <div className="container-fluid">
                             <div className="row text-center">
-                                <div className="col-12">
+                                {/* <div className="col-12">
                                     <h3 className="medium-title">
                                         Phone
                                     </h3>
-                                </div>
+                                </div> */}
                                 <div className="col-12">
                                     <img className="medium-imgs" src={Iphone} alt="Iphone" />
+                                </div>
+                            </div>
+                            <div className="row text-center">
+                                <div className="col-12">
+                                    <a href="tel:+17602381670" id="call-link">+1 (760) 238-1670</a>
                                 </div>
                             </div>
                         </div>
@@ -35,11 +40,11 @@ class ContactContainer extends Component {
                     <div className="col-4">
                         <div className="container-fluid">
                             <div className="row text-center">
-                                <div className="col-12">
+                                {/* <div className="col-12">
                                     <h3 className="medium-title">
                                         Email
                                     </h3>
-                                </div>
+                                </div> */}
                                 <div className="col-12">
                                     <img className="medium-imgs" src={Email} alt="Email" />
                                 </div>
@@ -49,11 +54,11 @@ class ContactContainer extends Component {
                     <div className="col-4">
                         <div className="container-fluid">
                             <div className="row text-center">
-                                <div className="col-12">
+                                {/* <div className="col-12">
                                     <h3 className="medium-title">
                                         Address
                                     </h3>
-                                </div>
+                                </div> */}
                                 <div className="col-12">
                                     <img className="medium-imgs" src={Location} alt="Address" />
                                 </div>
